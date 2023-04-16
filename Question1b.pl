a1c344cb846339386aa71d37bae34b137a1d4c0a
Men5ah <96393336+Men5ah@users.noreply.github.com>
2023-04-16 01:21:00 +0000

    Done, hopefully

--- Question1b.pl
+++ Question1b.pl
@@ -20,4 +20,22 @@ karasDates(Sol) :- length(Sol, 4),
     member((Vincent,_,_,_), Sol), member((Eddie,_,_,_), Sol), member((Zachary,_,_,_), Sol), member((Wayne,_,_,_), Sol),
     member((_,22,_,_), Sol), member((_,23,_,_), Sol), member((_,24,_,_), Sol), member((_,25,_,_), Sol),
     member((_,_,banker,_), Sol), member((_,_,firefighter,_), Sol), member((_,_,lawyer,_), Sol), member((_,_,teacher,_), Sol),
-    member((_,_,_,bowling alley), Sol), member((_,_,_,county fair), Sol), member((_,_,_,movies), Sol), member((_,_,_,restaurant), Sol),
+    member((_,_,_,bowling alley), Sol), member((_,_,_,county_fair), Sol), member((_,_,_,movies), Sol), member((_,_,_,restaurant), Sol),
+    
+    
+    %Clues
+    (member((Vincent,22,_,_), Sol);member((Vincent,23,_,_), Sol);member((Vincent,25,_,_), Sol)), %Clue1
+
+    (member((_,25,_,movies), Sol)), %clue2
+
+    (member((_,23,lawyer,_), Sol), member((Wayne,_,_,county_fair),Sol);member((Wayne,23,_,_), Sol), member((_,_,lawyer,county_fair),Sol)), %Clue3
+
+    (member((Eddie,_,lawyer,_), Sol);member((Eddie,_,_,movies), Sol)), %Clue4
+
+    (member((Eddie,A,_,_), Sol), member((_,B,_,restaurant), Sol), A<B), %Clue5
+
+    (member((Eddie,A,_,_), Sol), member((_,C,banker,_), Sol), C is A-1), %Clue6
+
+    member((_,_,firefighter,movies), Sol), %Clue7
+
+    (member((_,23,teacher,_), Sol);member((_,23,_,restaurant), Sol)), %Clue8
